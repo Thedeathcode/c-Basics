6eb160335aac5b2d57df6e8b9d60eb312a20bf2a
Thedeathcode <120058640+Thedeathcode@users.noreply.github.com>
2026-01-19 11:12:07 +0200

    problem solving

---
+++ Area of a Circle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+cout << fixed << setprecision(9);
+    float R;
+    double L=3.141592653;
+    cin >>R;
+
+   cout  <<L*(R*R);
+
+
+}

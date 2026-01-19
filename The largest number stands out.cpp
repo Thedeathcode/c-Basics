6ff147e6d331cad7a467b0eafc19cebf9f550b53
Thedeathcode <120058640+Thedeathcode@users.noreply.github.com>
2026-01-19 11:18:15 +0200

    project

---
+++ The largest number stands out.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int x,y;
+char a;
+cin>>x>>a>>y;
+if(x==5){cout<<"";}
+ else if(x>y){cout<<x<<">"<<y;}
+ else if(x<y){cout<<x<<"<"<<y;}
+ else if(x=y){cout<<x<<"="<<y;}
+
+
+
+
+}
+
+
+
+

32b35c58f04fd08c334e6023b1ef0e5dacc01d49
Thedeathcode <120058640+Thedeathcode@users.noreply.github.com>
2026-01-19 11:11:21 +0200

    problem solving

---
+++ Basic Data Types.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int a;
+    long long b;
+    char c;
+    float f;
+    double d;
+
+    cin >> a >> b >> c >> f >> d;
+    cout << a << "\n";
+    cout << b << "\n";
+    cout << c << "\n";
+    cout << f << "\n";
+    cout << d << "\n";
+
+
+
+}

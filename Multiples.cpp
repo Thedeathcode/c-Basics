b6a3b7821e78d90468e0d7f903ae542c0fc59558
Thedeathcode <120058640+Thedeathcode@users.noreply.github.com>
2026-01-19 11:12:46 +0200

    problem solving

---
+++ Multiples.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A,B;
+    cin >>A>>B;
+    if(B%A==0||A%B==0){cout<<"Multiples"<<"\n";}
+
+    else {cout <<"No Multiples"<<"\n";}
+}
